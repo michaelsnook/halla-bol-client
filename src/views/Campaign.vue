6d6b339b2fad07530fc04c90a14035797666ba4e
Michael Snook <snook@the-open.net>
2018-05-01 16:49:56 +0300

    Add campaigns page and sample form

---
+++ src/views/Campaign.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <p><h1>Campaign title</h1></p>
+        <p>here's a campaign body</p>
+      </div>
+      <div class="col-md-5 col-lg-4 my-lg-4 my-md-3">
+        <SingleForm />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SingleForm from '@/components/SingleForm.vue';
+
+export default {
+  name: 'campaign',
+  components: { SingleForm },
+};
+</script>
