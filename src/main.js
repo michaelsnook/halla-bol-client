6518dc55bce2be8dc86e7ad0d78aa6fb31762346
Michael Snook <snook@the-open.net>
2018-05-01 17:40:14 +0300

    Add main.scss with starting bootswatch theme

--- src/main.js
+++ src/main.js
@@ -3,8 +3,7 @@ import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import '@/assets/styles/main.scss';
 
 Vue.config.productionTip = false;
 
